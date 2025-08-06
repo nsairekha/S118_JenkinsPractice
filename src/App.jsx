a56cce531035c17ff22750623867101a73f8e217
nsairekha <rekhans321@gmail.com>
2025-08-06 10:32:16 +0530

    V1.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/S118_JenkinsPractice">
+    <Router basename="/ecommerce1">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
